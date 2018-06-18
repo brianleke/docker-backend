526a14482b6774e9da524978118629d7bcea4312
Brian Leke <brianleke@gmail.com>
2018-06-18 18:46:49 +0200

    Fix more sonar bugs

--- src/main/java/com/backend/TeamInfo.java
+++ src/main/java/com/backend/TeamInfo.java
@@ -1,7 +1,8 @@
 package java.com.backend;
 
 public class TeamInfo {
-    private String teamName, rawData;
+    private String teamName;
+    private String rawData;
 
     public TeamInfo(String teamName, String rawData) {
         this.teamName = teamName;
